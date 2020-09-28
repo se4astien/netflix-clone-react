8d4f0049a83c3b23ea61becc5fafdddf7299ea61
sebv <sebv@sv.home>
2020-09-28 22:11:42 +0200

    update

--- src/components/Row.js
+++ src/components/Row.js
@@ -7,7 +7,7 @@ const base_url = 'https://image.tmdb.org/t/p/original';
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState('');
+  // const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     async function fetchData() {
